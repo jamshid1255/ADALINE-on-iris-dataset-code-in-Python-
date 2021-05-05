89c60286e5e361e9eda343246cdedf4e966a88f9
jamshid1255 <jamshid1255@gmail.com>
2021-05-05 15:54:37 +0900

    ADALINE on iris dataset in Python

--- Iris dataset Adaline.py
+++ Iris dataset Adaline.py
@@ -35,6 +35,7 @@ w2=0.03
 error = 0.0
 errors=[]
 learningRate=0.0001;
+epoch = 2000
 def step(x):
     if (x > 0):
         return 1
@@ -42,25 +43,25 @@ def step(x):
         return -1;
 
 sum_square_error = []
+sum_square_error = [0 for i in range(epoch*100)]
 error_sum = 0 
-for j in range(2000):
+for j in range(epoch):
     for i in range(100):
         y=X[0][i]*w1+X[1][i]*w2+w0;
         pred = step(y)
-        error = (target[i] - y)
-        errors.append(error)
-        #error_sum = error_sum + (target[i] - pred) * np.array([[1,X[i][0],X[i][1]]]).transpose()
-        sum_square_error.append(((target[i] - pred)**2)/200)
+        
+        sum_square_error[i] = sum_square_error[i] + (((target[i] - pred)**2)/(2*100))
         
     w1 = w1 + learningRate*(target[i]-pred)*X[0][i];
     w2 = w2 + learningRate*(target[i]-pred)*X[1][i];
     w0 = w0 + learningRate*(target[i]-pred);
     
-
+print("Updated Weight w0", w0)
 print("Updated Weight w1", w1)
 print("Updated Weight w2", w2)
 print("Learning rate", learningRate)
 
+# testing data after training
 a = [5.1,4.9,4.7,6]
 b = [3.5,3,3.2,3.4]
 for i in range(4):
@@ -71,22 +72,10 @@ for i in range(4):
     print("Testing Data", a[i]," " , b[i],"  Results " , res)
 
 
-
-
-ax = plt.subplot(111)
-ax.plot(errors, c='#aaaaff', label='Training Errors')
-ax.set_xscale("log")
-plt.title("ADALINE Errors (2,-2)")
-plt.legend()
-plt.xlabel('Error')
-plt.ylabel('Value')
-plt.show()
-
-
 ax1 = plt.subplot(111)
 ax1.plot(sum_square_error, c='#aaaaff', label='Sum Square Error')
 ax1.set_xscale("log")
-plt.title("ADALINE SS Errors (2,-2)")
+plt.title("ADALINE Sum Square Errors ")
 plt.legend()
 plt.xlabel('Error')
 plt.ylabel('Value')
